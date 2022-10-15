4a36fd15fbdb902767b9e2ef64aec9372254bdef
EmilFunding <47417151+EmilFunding@users.noreply.github.com>
2022-10-15 14:02:42 +0200

    Board

--- oo/src/board.ts
+++ oo/src/board.ts
@@ -15,13 +15,50 @@ export type BoardEvent<T> = ?;
 export type BoardListener<T> = ?;
 
 export class Board<T> {
+
+    width : number;
+    height : number;
+    generator : Generator<T>;
+
+    pieces : T[];
+
+    constructor(generator: Generator<T>, width : number, height : number) {
+        this.generator = generator;
+        this.width = width;
+        this.height = height;
+
+        //Fill the board
+        var boardSize = width * height;
+        for (let index = 0; index < boardSize; index++) {
+            this.pieces[index] = generator.next();
+        }
+    }
+
+    IndexToPosition(i : number) : Position {
+        const pos : Position;
+        pos.col = i % this.width;
+        pos.row = i / this.width;
+        return pos;
+    }
+    PositionToIndex(position : Position) : number {
+        return position.col + position.row * this.width;
+    }
+      
     addListener(listener: BoardListener<T>) {
     }
 
     piece(p: Position): T | undefined {
+        return this.pieces[this.PositionToIndex(p)];
     }
 
     canMove(first: Position, second: Position): boolean {
+
+        //Check row
+        
+
+        //Check col
+
+        return true;
     }
     
     move(first: Position, second: Position) {
