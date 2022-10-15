b3e0d5d31bd21bf2e3a74ec18436cafb7c01ea75
Oliver Brodersen <brodersen88@gmail.com>
2022-10-15 14:45:18 +0200

    canMove

--- oo/src/board.ts
+++ oo/src/board.ts
@@ -51,14 +51,43 @@ export class Board<T> {
         return this.pieces[this.PositionToIndex(p)];
     }
 
+    getMatchedPieces(first: Position, second: Position): Position[]{
+        let positions: Position[];
+        const horizontalMatch = first.col == second.col + 1 || second.col - 1; 
+        const verticalMatch = first.row == second.row + 1 || second.row - 1; 
+
+        if(!horizontalMatch && !verticalMatch)
+            return positions;
+
+        const piece = this.piece(first);
+
+        // Checks two tiles to each side of the second position 
+        // since any more matching tiles would have already resulted in a match event.
+        for(let i = -2; i++; i < 2){
+            // I is 0 when the second position is checked. We know this piece is not a match so we skip it.
+            if(i == 0)
+                continue;
+            
+            const pos : Position;
+            pos.col = horizontalMatch ? second.col : second.col + i;
+            pos.row = horizontalMatch ? second.row + i : second.row;
+
+            if(this.piece(pos) == piece){
+                positions.push(pos);
+            }    
+            else{
+                positions.splice(0);
+            }
+        }
+
+        // Adds second position to array since it will always be a match as long as the move is legal.
+        // This makes the function reusable getting matched positions for the 'Match' event
+        positions.push(second);
+        return positions;
+    }
+
     canMove(first: Position, second: Position): boolean {
-
-        //Check row
-        
-
-        //Check col
-
-        return true;
+        return this.getMatchedPieces(first, second).length >= 3;
     }
     
     move(first: Position, second: Position) {
